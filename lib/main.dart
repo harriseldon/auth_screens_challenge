5cd5fbca0d5a5c6bc0f53c5f2af45ad1d35969c8
dgreengas <dave.greengas@gmail.com>
2021-04-28 10:01:47 -0500

    Initial Clone

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+final signupPurpleGradient = LinearGradient(colors: [
+  Color.fromRGBO(145, 131, 222, 1),
+  Color.fromRGBO(160, 148, 227, 1),
+]);
+final signupDarkPurple = Color.fromRGBO(82, 67, 154, 1);
+
+final loginPurpleGradient = LinearGradient(colors: [
+  Color.fromRGBO(229, 178, 202, 1),
+  Color.fromRGBO(205, 130, 222, 1),
+]);
+final loginDarkPurple = Color.fromRGBO(120, 37, 139, 1);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Auth Screen Challenge',
+      home: SignUpScreen(),
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
